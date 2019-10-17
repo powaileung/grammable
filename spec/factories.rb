857bec2441fb2dcf36f059302c1bc73f143f4a38
Powai Leung <pwleung1489@gmail.com>
2019-10-17 21:12:37 +0000

    input TDD Lesson 11 Adding FactoryBot

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do 
   factory :user do
     sequence :email do |n|
-      { "dummyEmail#{n}@gmail.com" }
+       "dummyEmail#{n}@gmail.com" 
     end
-    password { "secretPassword" }
-    password_confirmation { "secretPassword" }
+    password  { "secretPassword" }
+    password_confirmation  { "secretPassword" }
     end
   end
-end
+
