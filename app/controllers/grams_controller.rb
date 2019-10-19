931b8ed3e37a824be8e943597727c3b6fb74a2e7
Powai Leung <pwleung1489@gmail.com>
2019-10-19 02:20:12 +0000

    input TDD Lesson 16 CarrierWave

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,9 +1,11 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    
     @gram.destroy
     redirect_to root_path
   end
@@ -11,9 +13,9 @@ class GramsController < ApplicationController
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
-    
     if @gram.valid?
       redirect_to root_path
     else
@@ -26,11 +28,13 @@ class GramsController < ApplicationController
   end
 
   def index
+    @grams = Gram.all
   end
 
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def show
@@ -50,11 +54,11 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
   end
 
 end
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,7 @@
-class Gram < ApplicationRecord
+class Gram < ActiveRecord::Base
+  mount_uploader :picture, PictureUploader
   validates :message, presence: true
 
   belongs_to :user
+
 end
