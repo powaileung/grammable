df4048f565d9a0dcbb4442fa3affda0b2fea2b33
Powai Leung <pwleung1489@gmail.com>
2019-10-22 04:34:49 +0000

    input TDD Lesson 18 Add Comments

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,8 @@
-class Gram < ActiveRecord::Base
-  mount_uploader :picture, PictureUploader
+class Gram < ApplicationRecord
   validates :message, presence: true
+  validates :picture, presence: true
+
+  mount_uploader :picture, PictureUploader
 
   belongs_to :user
 
