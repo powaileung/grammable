38758919ed41ae13365fab993425c75e83d7a9ba
Powai Leung <pwleung1489@gmail.com>
2019-10-22 05:16:26 +0000

    input TDD Lesson 18 Add Comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ApplicationRecord
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, uyou may want to us :null_session instead.
+  # protect_from_forgery with: :exception
+
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
 end
